1f35080de941e51d00c6ef3cf0cfed746e483647
George Nguyen <ghnguyen91@gmail.com>
2018-06-04 09:40:57 -0500

    post images setup complete

--- server/src/routes/image.js
+++ server/src/routes/image.js
@@ -2,16 +2,21 @@ import { Router } from 'express';
 import * as cloudinary from 'cloudinary';
 import { config } from '../config';
 import Table from '../table';
+import { user } from '../config/passport';
+import { tokenMiddleware } from '../middleware/auth.mw';
+import { callProcedure } from '../config/db';
 
 let router = Router();
 const imagesTable = new Table('images');
-const userImagesTable = new Table('userimages');
+const userImagesTable = new Table('usersimages');
 
 cloudinary.config({
     cloud_name: "hxkggeeaw",
     api_key: config.CLOUDINARY_API_KEY,
     api_secret: config.CLOUDINARY_API_SECRET
 })
+
+router.use(tokenMiddleware);
 
 router.post('/', (req, res) => {
     console.log('before Cloudinary')
@@ -20,14 +25,20 @@ router.post('/', (req, res) => {
             console.log(results)
             return imagesTable
                 .insert({ url: results.secure_url })
+        }).then((results) => {
+            return userImagesTable
+                .insert({ userid: req.user.id, imageid: results.id })
         }).then(() => {
-            res.sendStatus(201)
-        })
-        .catch(err => {
+            res.sendStatus(201);
+        }).catch(err => {
             console.log(err);
             res.sendStatus(500);
         })
 });
+
+router.get('/:user_id', (req, res) => {
+    callProcedure()
+})
 
 // router.get('/:img_id', (req, res) => {
 //     cloudinary.url(req.params.img_id)
